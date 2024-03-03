ba7942772ae12f9340a351ee1323efe3902181a6
Omkar Vasekar <125749197+omkarvasekar@users.noreply.github.com>
2024-03-03 22:37:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -41,11 +41,11 @@ function App() {
       <h1 className='text-4xl text-center text-white'>Password Generator</h1>
       <div className='w-full  max-w-md mx-auto shadow-md rounded-lg px-4 my-8  bg-gray-700'>
         <h1 className='text-white text-center my-4'>Password generator</h1>
-        <div className='flex items-center flex-wrap shadow rounded-lg overflow-hidden mb-4'>
+        <div className='flex  shadow rounded-lg overflow-hidden mb-4'>
           <input type='text' value={password} readOnly ref={passwordRef} className='outline-none w-full py-1 px-3 text-black' placeholder='Password' />
           <button onClick={CopytoClip} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 transition ease-in-out delay-150hover:-translate-y-1 hover:scale-110 duration-300 '  >Copy</button>
         </div>
-        <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center flex-wrap text-sm gap-x-2'>
           <div className='flex items-center gap-x-1 py-1 my-2 mx-2'>
             <input type='range' min={6} max={20} value={length} className='cursor-pointer' onChange={(e) => { setlength(e.target.value) }} />
             <label className='text-white'>Length:{length}</label>
